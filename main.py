08061d0bd1e185ee1f7ea6464bdc7cc3c5136991
Ture Claussen <ture.cl.business@gmail.com>
2022-02-08 18:19:12 +0100

    Refactor for multi command environment

--- main.py
+++ main.py
@@ -12,15 +12,21 @@ coloredlogs.install(level='INFO')
 
 def main() -> int:
     parser = argparse.ArgumentParser(description='Manage your movie tables')
-    parser.add_argument('master_table_path', type=str,
-                        help='The path of the master table')
-    parser.add_argument('supplementary_table_path', type=str,
-                        help='The path of the supplementary table to be merged into the master table')
     parser.add_argument('--interactive', help='Ask before taking actions',
                         action='store_false', default=True)
+    subparsers = parser.add_subparsers(dest='command')
+
+    parser_merge = subparsers.add_parser(
+        'merge', help='Merge a supplementary movie table into a master table')
+    parser_merge.add_argument('master_table_path', type=str,
+                              help='The path of the master table')
+    parser_merge.add_argument('supplementary_table_path', type=str,
+                              help='The path of the supplementary table to be merged into the master table')
+
     args = parser.parse_args()
 
-    controllers.merge_tables(args)
+    if (args.command == 'merge'):
+        controllers.merge_tables(args)
 
 
 if __name__ == '__main__':
